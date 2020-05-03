cf7363b4168217435cc54052e73ec25fede21b6f
weilyuwang <weilyuwang@gmail.com>
2020-05-03 18:39:31 -0400

    small refactor

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -4,7 +4,7 @@ import { StyledCell } from "./styles/StyledCell";
 import { TETROMINOS } from "../tetrominos";
 
 const Cell = ({ type }) => (
-    <StyledCell type={"L"} color={TETROMINOS["L"].color} />
+    <StyledCell type={type} color={TETROMINOS[type].color} />
 );
 
 export default Cell;
