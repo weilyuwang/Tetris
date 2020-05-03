c9e2d42b5cb992fa447eacb9c6686bc387767803
weilyuwang <weilyuwang@gmail.com>
2020-05-03 18:40:38 -0400

    adjust cell size

--- src/components/styles/StyledStage.js
+++ src/components/styles/StyledStage.js
@@ -4,12 +4,12 @@ export const StyledStage = styled.div`
     display: grid;
     grid-template-rows: repeat(
         ${(props) => props.height},
-        calc(40vw / ${(props) => props.width})
+        calc(28vw / ${(props) => props.width})
     );
     grid-template-columns: repeat(${(props) => props.width}, 1fr);
     grid-gap: 1px;
     border: 2px solid #333;
     width: 100%;
-    max-width: 40vw;
+    max-width: 28vw;
     background: #111;
 `;
